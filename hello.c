41425fe6c5ee27bfbaa3cd3aee3aa57f17fa42ba
MANYARAMAANGI <102029562+MANYARAMAANGI@users.noreply.github.com>
2022-03-26 17:12:35 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,22 @@
+ // program to find the sum of five numbers
+ # include <stdio.h>
+ int main ()
+ {
+    int j=13;
+    int o=19;
+    int s=23;
+    int i=25;
+    int a=29;
+    int sum;
+    sum=(j+o+s+i+a);
+    printf("the sum is %d",sum );
+    return 0;
+ 
+ }
+
+
+
+
+
+
+
