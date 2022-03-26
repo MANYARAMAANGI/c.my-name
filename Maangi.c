41d1df4b32280c3db16c8a35e606c0b680ba4232
MANYARAMAANGI <102029562+MANYARAMAANGI@users.noreply.github.com>
2022-03-26 17:06:59 +0300

    Add files via upload

---
+++ Maangi.c
@@ -0,0 +1,12 @@
+// program that accepts two integers from the user and the products of the two integers
+# include <stdio.h>
+int main ()
+{
+int s,t,p;
+printf("\n enter the first number:");
+scanf("%d",&s);
+printf("\n enter the second number:");
+scanf("%d",&t);
+p=s*t;
+printf("the product is %d\n",p);
+}
